8326ca63bb409e03af4afdf7e8768bf5af450355
SpringSrikanth <rajudusa59@gmail.com>
2022-09-03 01:56:15 +0530

    Added game-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'games-app';
+
+  public games:any[]=["Foot Ball","Volley Ball","Cricket","Hockey"];
+  public selectedGame:any="";
+  inputValue:string="";
+
+  constructor(){}
+
+  selectGame(i:any){
+    this.selectedGame=i;
+  }
+  addGame(){
+    this.games.push(this.inputValue);
+    this.inputValue="";
+    console.log(this.games); 
+  }
+  removeGame(i:any){
+    this.games.splice(i,1);
+  }
 }
